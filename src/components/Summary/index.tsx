c9787e472320d22d2059942b275a978e89493dcd
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-03-06 16:14:52 -0300

    feat: implements format for prices and dates

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,6 +1,7 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { useContext } from "react";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { priceFormatter } from "../../utils/formatter";
 import { SummaryCard, SummaryContainer } from "./styles";
 
 export function Summary() {
@@ -32,7 +33,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color={"#0664BB"} />
         </header>
 
-        <strong>{summary.income}</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -41,7 +42,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color={"#F75A68"} />
         </header>
 
-        <strong>{summary.outcome}</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
 
       <SummaryCard variant="blue">
@@ -50,7 +51,7 @@ export function Summary() {
           <CurrencyDollar size={32} color={"#FFFFFF"} />
         </header>
 
-        <strong>{summary.total}</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
